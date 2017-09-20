ca379bf956d3c6b460bd3afe5dd40968dfef71a6
Alicia Gonzalez <alicia.gonzalez.90@gmail.com>
2017-09-20 21:04:36 +0200

    Adds volunteers section to header

--- src/client/components/header.tsx
+++ src/client/components/header.tsx
@@ -9,6 +9,7 @@ export class MainHeader extends React.Component<any, any> {
             <h2 className="logo">¿Como Ayudar?</h2>
             <nav className="mainNav">
               <a className="modal__link" href="#" onClick={ () => this.props.show_about() } data-modal="about">Acerca de</a>
+              <Link className="hide-mobile"  to="/volunteers">Voluntarios</Link>
               <Link className="hide-mobile"  to="/contact">Contactos de Emergencia</Link>
               <a className="hide-mobile" href="https://github.com/eldelentes/comoayudarmx" target="_blank">Contribuir <i className="fa fa-github"></i></a>
             </nav>
